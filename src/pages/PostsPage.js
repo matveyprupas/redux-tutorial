9e6ae02951cd159a4eb3a5b10c9ca913b58cce68
matveyprupas <matveyprupas@gmail.com>
2021-09-20 22:21:09 +0300

    fix: some code added

---
+++ src/pages/PostsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+const PostsPage = () => {
+    return (
+        <section>
+            <h1>Posts</h1>
+            <p>This is a posts list.</p>
+        </section>
+    );
+};
+
+export default PostsPage;
